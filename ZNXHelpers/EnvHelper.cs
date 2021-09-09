a4a50b8f4c2a4f0c29ca621ac03c2e7d6434af7e
Kenneth Soh <kennethsohyq@gmail.com>
2021-09-09 22:02:34 +0800

    Initial Commit with existing repo

---
+++ ZNXHelpers/EnvHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace ZNXHelpers
+{
+    public static class EnvHelper
+    {
+        public static string GetString(string key)
+        {
+            return Environment.GetEnvironmentVariable(key);
+        }
+
+        public static string GetString(string key, string defaultValue)
+        {
+            var envRes = GetString(key);
+            if (string.IsNullOrEmpty(envRes))
+            {
+                envRes = defaultValue;
+            }
+            return envRes;
+        }
+
+        public static int GetInt(string key)
+        {
+            var envVar = GetString(key);
+            return int.Parse(envVar);
+        }
+
+        public static int GetInt(string key, int defaultValue)
+        {
+            int envRes;
+            try
+            {
+                envRes = GetInt(key);
+            }
+            catch (Exception)
+            {
+                envRes = defaultValue;
+            }
+
+            return envRes;
+        }
+
+        public static long GetLong(string key)
+        {
+            var envVar = GetString(key);
+            return long.Parse(envVar);
+        }
+
+        public static long GetLong(string key, long defaultValue)
+        {
+            long envRes;
+            try
+            {
+                envRes = GetLong(key);
+            }
+            catch (Exception)
+            {
+                envRes = defaultValue;
+            }
+
+            return envRes;
+        }
+
+        public static double GetDouble(string key)
+        {
+            var envVar = GetString(key);
+            return double.Parse(envVar);
+        }
+
+        public static double GetDouble(string key, double defaultValue)
+        {
+            double envRes;
+            try
+            {
+                envRes = GetDouble(key);
+            }
+            catch (Exception)
+            {
+                envRes = defaultValue;
+            }
+
+            return envRes;
+        }
+
+        public static bool GetBool(string key)
+        {
+            var envVar = GetString(key);
+            return bool.Parse(envVar);
+        }
+
+        public static bool GetBool(string key, bool defaultValue)
+        {
+            bool envRes;
+            try
+            {
+                envRes = GetBool(key);
+            }
+            catch (Exception)
+            {
+                envRes = defaultValue;
+            }
+
+            return envRes;
+        }
+    }
+}

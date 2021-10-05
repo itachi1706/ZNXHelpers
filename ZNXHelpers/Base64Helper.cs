b805466bf700071390440c9fa05a177c7efa2905
Julian Chua Wee Kuang (KLSG) <julian.chua@kydonlsg.com>
2021-10-05 22:37:24 +0800

    Added Base64Helper and JwtHelper

---
+++ ZNXHelpers/Base64Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace ZNXHelpers
+{
+    public class Base64Helper
+    {
+        public string EncodeString(string input)
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            return Convert.ToBase64String(inputBytes);
+        }
+
+        public string DecodeString(string input)
+        {
+            byte[] inputBytes = Convert.FromBase64String(input);
+            return Encoding.UTF8.GetString(inputBytes);
+        }
+    }
+}
---
+++ ZNXHelpers/JwtHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace ZNXHelpers
+{
+    public class JwtHelper
+    {
+        public bool ValidateJwt(string token, TokenValidationParameters tokenValidationParameters, out List<Claim> claims, out SecurityToken securityToken)
+        {
+            try
+            {
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                ClaimsPrincipal claimsPrincipal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+                claims = new List<Claim>(claimsPrincipal.Claims);
+                return true;
+            } catch
+            {
+                claims = null;
+                securityToken = null;
+                return false;
+            }
+        }
+    }
+}

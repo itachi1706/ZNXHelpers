4a4c2dc1ee308aab4a187eba910b247a8a5f960d
Julian Chua Wee Kuang (KLSG) <julian.chua@kydonlsg.com>
2021-10-14 12:08:14 +0800

    Added RngHelper

---
+++ ZNXHelpers/RngHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ZNXHelpers
+{
+    public class RngHelper
+    {
+        public string GeneratePassword(int size)
+        {
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] buffer = new byte[size];
+                rng.GetBytes(buffer);
+                string result = Convert.ToBase64String(buffer);
+                return result;
+            }
+        }
+    }
+}

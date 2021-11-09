3823b84eb363f41462f57aa8ec303153ae8ab732
Julian Chua Wee Kuang (KLSG) <julian.chua@kydonlsg.com>
2021-11-09 18:22:50 +0800

    Added new methods creating Jwt(JWS) tokens, upgrade to 1.2.11

--- ZNXHelpers/JwtHelper.cs
+++ ZNXHelpers/JwtHelper.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ZNXHelpers
 {
@@ -21,6 +23,33 @@ namespace ZNXHelpers
                 securityToken = null;
                 return false;
             }
+        }
+
+        public string CreateJws(
+            string issuer = null,
+            string audience = null,
+            IEnumerable<Claim> claims = null,
+            DateTime? notBefore = null,
+            DateTime? expires = null,
+            X509Certificate2 signingCertificate = null)
+        {
+            X509SigningCredentials signingCredentials = new X509SigningCredentials(signingCertificate, "RS256");
+
+            return CreateJws(issuer, audience, claims, notBefore, expires, signingCredentials);
+        }
+
+        public string CreateJws(
+            string issuer = null,
+            string audience = null,
+            IEnumerable<Claim> claims = null,
+            DateTime? notBefore = null,
+            DateTime? expires = null,
+            SigningCredentials signingCredentials = null)
+        {
+            JwtSecurityToken jwtSecurityToken = new JwtSecurityToken(issuer, audience, claims, notBefore, expires, signingCredentials);
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            string token = handler.WriteToken(jwtSecurityToken);
+            return token;
         }
     }
 }

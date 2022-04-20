e906e47c141202486663ef190437a820bd7fd134
Julian Chua <julian.chua@zionext.com>
2022-04-20 15:54:56 +0800

    Added custom HttpResponseException

---
+++ ZNXHelpers/Exceptions/HttpResponseException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.Serialization;
+namespace ZNXHelpers.Exceptions
+{
+    [Serializable]
+    public class HttpResponseException : Exception
+    {
+        public int StatusCode { get; }
+        public string CustomMessage { get; }
+
+        public HttpResponseException(int statusCode)
+        {
+            StatusCode = statusCode;
+        }
+
+        public HttpResponseException(int statusCode, string customMessage)
+        {
+            StatusCode = statusCode;
+            CustomMessage = customMessage;
+        }
+
+        protected HttpResponseException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public override string Message
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(CustomMessage))
+                {
+                    return $"HTTP Response Status Code {StatusCode}.";
+                }
+                return $"{CustomMessage} : HTTP Response Status Code {StatusCode}.";
+            }
+        }
+    }
+}
